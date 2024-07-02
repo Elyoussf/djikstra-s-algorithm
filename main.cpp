84f67777fa7e3956de98b4526c0c271f7a137313
Elyoussf <strangeamazigh1968@gmail.com>
2024-07-02 13:43:36 +0100

    life is good

--- main.cpp
+++ main.cpp
@@ -5,54 +5,7 @@
 
 using namespace std;
 
-struct CompareFirst {
-    bool operator()(const vector<int>& a, const vector<int>& b) {
-        return a[1] < b[1]; // Min-heap: smallest distance has the highest priority
-    }
-};
 
-// int main() {
-//     int inf = numeric_limits<int>::max();
-
-//     // Graph represented as an adjacency list with weights
-//     vector<vector<pair<int, int>>> graph = {
-//         {{1, 1}, {2, 3}},    // Neighbors for node 0
-//         {{0, 1}, {4, 0}},    // Neighbors for node 1
-//         {{0, 3},{3,4},{4,1}},            // Neighbors for node 2
-//         {{2, 4}},            // Neighbors for node 3
-//         {{1,0},{2,1}}                   // Neighbors for node 4
-//     };
-
-//     vector<int> distances(5, inf);
-//     distances[0] = 0;
-//     int source = 0;
-//     priority_queue<vector<int>, vector<vector<int>>, CompareFirst> pq;
-//     pq.push({0, 0});
-    
-
-//     while (!pq.empty()) {
-//         int dist = pq.top()[1];
-//         int node = pq.top()[0];
-//         pq.pop();
-
-       
-
-//         for (auto& edge : graph[node]) {
-//             int neighbor = edge.first;
-//             int weight = edge.second;
-//             if (distances[node] + weight < distances[neighbor]) {
-//                 distances[neighbor] = distances[node] + weight;
-//                 pq.push({neighbor,distances[neighbor]});
-//             }
-//         }
-//     }
-
-//     for (int i = 0; i < distances.size(); ++i) {
-//         cout << "The distance to node " << i << " is: " << distances[i] << endl;
-//     }
-
-//     return 0;
-// }
 struct customComparator{
     bool operator()(const vector<int> &a,const vector<int> &b){
         return a[1]<b[1];
